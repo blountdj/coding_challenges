3b8e0a555415a69b8034f9824ced9d574312c91a
blountdj <blountdj@gmail.com>
2022-07-04 13:18:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleClass() {
-  document.getElementById("subnav-content").classList.toggle("show");
-  document.getElementById("dropdown-arrow").classList.toggle("up");
-}
-// Add event listener to dropdown
-document.getElementById("subnavbtn").addEventListener("click", toggleClass);
